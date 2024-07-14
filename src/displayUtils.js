5e69dee4a241641faf418b3d8b67d2d9d6f3d5c4
p4pirate <dansanakoshal05@gmail.com>
2024-07-14 20:16:50 +0530

    Update displayUtils.js

--- src/displayUtils.js
+++ src/displayUtils.js
@@ -4,8 +4,8 @@ function displayHeader() {
   process.stdout.write('\x1Bc');
   console.log('========================================'.cyan);
   console.log('=           Sonic Odyssey BOT          ='.cyan);
-  console.log('=     Created by PIRATE      ='.cyan);
-  console.log('=    https://t.me/crypto_airdrop_pirates     ='.cyan);
+  console.log('=              Created by PIRATE       ='.cyan);
+  console.log('= https://t.me/crypto_airdrop_pirates  ='.cyan);
   console.log('========================================'.cyan);
   console.log();
 }
